3ccf1bd19b803716777b278d316422d920717ef3
rrrzzzqqq <dahlanrizky@gmail.com>
2026-08-06 21:05:24 +0700

    tambah tomobl hapus dan reset

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const tombolTambah = document.getElementById("tombolTambah");
+const reset = document.getElementById("reset");
 const inputan = document.querySelector(".inputan");
 const wadahJadwal = document.getElementById("wadahJadwal");
 
 tombolTambah.addEventListener("click", () => {
   if (inputan.value.trim() === "") return;
+  const wadahPerJadwalDanButton = document.createElement("div");
+  wadahPerJadwalDanButton.className = "wadahJadwalSebaris";
   const tambahJadwal = document.createElement("p");
+  const tombolMinusPerJadwal = document.createElement("button");
+  tombolMinusPerJadwal.className = "tombolMinusPerJadwal";
+  tombolMinusPerJadwal.textContent = "-";
   tambahJadwal.textContent = inputan.value;
-  wadahJadwal.appendChild(tambahJadwal);
+  wadahPerJadwalDanButton.appendChild(tambahJadwal);
+  wadahPerJadwalDanButton.appendChild(tombolMinusPerJadwal);
+  wadahJadwal.appendChild(wadahPerJadwalDanButton);
   inputan.value = "";
+
+  tombolMinusPerJadwal.addEventListener("click", () => {
+    if (wadahPerJadwalDanButton.lastElementChild)
+      wadahPerJadwalDanButton.lastElementChild.remove();
+    if (wadahPerJadwalDanButton.lastElementChild)
+      wadahPerJadwalDanButton.lastElementChild.remove();
+  });
+});
+
+reset.addEventListener("click", () => {
+  if (wadahJadwal) wadahJadwal.remove();
 });
