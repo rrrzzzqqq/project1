b7b4d1a9695cc5cba0fd67d7c4a846c64dd159dd
rrrzzzqqq <dahlanrizky@gmail.com>
2026-08-16 00:45:42 +0700

    tambah komentar

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const inputan = document.querySelector(".inputan");
 const wadahJadwal = document.getElementById("wadahJadwal");
 
 tombolTambah.addEventListener("click", () => {
+  // jika inputan kosong, maka kembali ke awal
   if (inputan.value.trim() === "") return;
+  // wadah untuk jadwal per-baris
   const wadahPerJadwalDanButton = document.createElement("div");
+  // deklarasi nama kelas
   wadahPerJadwalDanButton.className = "wadahJadwalSebaris";
   const tambahJadwal = document.createElement("p");
+  // deklarasi jadwal.
   const tombolMinusPerJadwal = document.createElement("button");
   tombolMinusPerJadwal.className = "tombolMinusPerJadwal";
   tombolMinusPerJadwal.textContent = "-";
@@ -17,19 +21,23 @@ tombolTambah.addEventListener("click", () => {
   wadahJadwal.appendChild(wadahPerJadwalDanButton);
   inputan.value = "";
 
+  // hapus per-jadwal
   tombolMinusPerJadwal.addEventListener("click", () => {
     if (wadahPerJadwalDanButton) wadahPerJadwalDanButton.remove();
   });
 
+  // mencoret per-jadwal
   wadahPerJadwalDanButton.addEventListener("click", () => {
     wadahPerJadwalDanButton.classList.toggle("coret");
   });
 });
 
+// hapus semua jadwal
 reset.addEventListener("click", () => {
   if (wadahJadwal) wadahJadwal.innerHTML = "";
 });
 
+// tambah per-jadwal dengan tombol enter
 inputan.addEventListener("keypress", (enter) => {
   if (enter.key === "Enter") {
     tombolTambah.click();
